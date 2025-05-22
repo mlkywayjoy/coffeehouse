404e97ad4291be624f3f40eeccb56b84df0ecd13
mlkywayjoy <ivislytan@gmail.com>
2025-05-22 16:04:04 +0800

    Initial commit

---
+++ coffeehouse/script.js
@@ -0,0 +1,5 @@
+document.getElementById('orderForm').addEventListener('submit', function (e) {
+    e.preventDefault();
+    alert('Order submitted! Please check your email for confirmation.');
+  });
+  
